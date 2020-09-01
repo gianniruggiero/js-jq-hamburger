7937116c8300266e420bfed919c6960302e89435
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-01 16:53:39 +0200

    usato classe active per display:block / selector per chiusura .close

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 var ico_hamburger = $('.header-right a');
-var menu_hamburger = $('.hamburger-menu')
-var ico_close_menu_hamburger = $('.hamburger-menu a .fa-times')
+var menu_hamburger = $('.hamburger-menu');
+var ico_close_menu_hamburger = $('.close');
 
 ico_hamburger.click(
   function () {
     menu_hamburger.addClass("active");
-    // menu_hamburger.position(20, 0);
   }
 )
 
