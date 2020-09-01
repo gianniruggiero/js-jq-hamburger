e0c72d05545b596f7689b853ac220dc9fc541e1f
GIanni Ruggiero <gianni@groovecompany.it>
2020-09-01 16:00:39 +0200

    completato v.2

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 var ico_hamburger = $('.header-right a');
 var menu_hamburger = $('.hamburger-menu')
-var ico_close_menu_hamburger = $('.fa-times')
+var ico_close_menu_hamburger = $('.hamburger-menu a .fa-times')
 
 ico_hamburger.click(
   function () {
-    menu_hamburger.show(200);
+    menu_hamburger.addClass("close");
   }
 )
 
 ico_close_menu_hamburger.click(
   function () {
-    menu_hamburger.hide(200);
+    menu_hamburger.removeClass("close");
   }
 )
